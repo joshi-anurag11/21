4a8ecea273b0bc232864994b9572591212774c62
Anurag Joshi <anurag2711@gmail.com>
2020-08-12 02:42:51 +0530

    Dialog box

--- Firstapp/main11.py
+++ Firstapp/main11.py
@@ -1,10 +1,11 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivymd.uix.list import OneLineListItem
+from kivymd.uix.textfield import MDTextField
 from kivymd.uix.dialog import MDDialog
 from kivy.core.window import Window
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
 from kivymd.uix.button import MDFlatButton
 from kivy.uix.screenmanager import ScreenManager, Screen
 
@@ -26,8 +27,6 @@ Screen:
             MDList:
                 id:container
                 
-    
-            
         MDFloatLayout:
         
             MDFloatingActionButton:
@@ -39,7 +38,7 @@ Screen:
                 pos_hint: {'center_x':0.5,'center_y':0.2}
                 elevation: 8
                 on_release: app.show_dialog()
-                                
+
 <content>:
     id:content
     orientation:'vertical'
@@ -49,14 +48,11 @@ Screen:
     MDTextField:
         id: user_text
         hint_text:'Enter your dream'
-   
-                         
+        required: True                                                      
 '''
-
 
 class content(MDBoxLayout):
     pass
-
 
 class MainApp(MDApp):
     def build(self):
@@ -65,12 +61,13 @@ class MainApp(MDApp):
 
     def show_dialog(self):
         self.dialog = MDDialog(title='Dream',
+                               auto_dismiss=False,
                                type='custom',
                                content_cls=content(),
                                size_hint=(0.8, 0.3),
                                buttons=[
                                    MDFlatButton(text='CANCEL', on_release=self.close),
-                                   MDFlatButton(text='OK', on_release=self.user_content)
+                                   MDFlatButton(text='OK', on_release=self.add_list)
                                ]
                                )
         self.dialog.open()
@@ -78,14 +75,14 @@ class MainApp(MDApp):
     def close(self, obj):
         self.dialog.dismiss()
 
-    def user_content(self,obj):
-        user_text=self.content.user_text
-        print(user_text)
-
     def add_list(self, obj):
-        #item = OneLineListItem(text=)
-        #self.root.ids.container.add_widget(item)
-        self.dialog.dismiss()
+        user_text = self.dialog.content_cls.ids.user_text.text
+        if len(user_text) > 0:
+            item = OneLineListItem(text=user_text)
+            self.root.ids.container.add_widget(item)
+            self.dialog.dismiss()
+        else:
+            pass
 
 
 MainApp().run()
